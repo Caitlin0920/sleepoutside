e64778de197d1d8b0fc67f262ca638237300f796
DanielGRasmussen <104102130+DanielGRasmussen@users.noreply.github.com>
2023-01-06 11:43:30 -0700

    Forgot to lint & format

--- src/js/product.js
+++ src/js/product.js
@@ -4,11 +4,11 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  let cart = getLocalStorage("so-cart")
+  let cart = getLocalStorage("so-cart");
   if (cart == null) {
     cart = [];
   }
-  cart.push(product)
+  cart.push(product);
   setLocalStorage("so-cart", cart);
 }
 // add to cart button event handler
