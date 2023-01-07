f284273757731fc1da89b49dd4f030b1a1cc1dff
ryder4676 <60828482+ryder4676@users.noreply.github.com>
2023-01-06 17:57:44 -0700

    sucessfully got item to show up in cart

--- src/js/product.js
+++ src/js/product.js
@@ -4,8 +4,17 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let cart = setLocalStorage("so-cart", product);
+
+  if (cart == null) {
+    cart = [];
+  }
+  cart.push(product);
+  setLocalStorage("so-cart", cart);
 }
+
+
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
