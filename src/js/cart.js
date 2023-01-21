cff70945ede34e5fa700b8372963170fa7b20c25
Yun-Ting Chang <cha22033@byui.edu>
2023-01-21 12:00:08 -0700

    Cart quantities

--- src/js/cart.js
+++ src/js/cart.js
@@ -5,6 +5,8 @@ function renderCartContents() {
   if (cartItems) {
     const htmlItems = cartItems.map((item) => cartItemTemplate(item));
     document.querySelector(".product-list").innerHTML = htmlItems.join("");
+    calculateTotal(cartItems);
+    document.querySelector(".cart-footer").style.display = "block";
   } else {
     document.querySelector(".product-list").innerHTML = "Empty";
   }
@@ -22,11 +24,42 @@ function cartItemTemplate(item) {
     <h2 class="card__name">${item.Name}</h2>
   </a>
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
+  <p>qty:<input type="number" class="cart-card__quantity" value="1" min="1"></p>
+  <p>$<span class="cart-card__price">${item.FinalPrice}</span></p>
 </li>`;
 
   return newItem;
+}
+
+function calculateTotal(cartItems) {
+  const qty = document.getElementsByClassName("cart-card__quantity");
+  const itemPrice = document.getElementsByClassName("cart-card__price");
+  let totalPrice = 0;
+  const totalPriceTag = document.querySelector(".cart-total");
+
+  for (let i = 0; i < cartItems.length; i++) {
+    qty[i].setAttribute("id", `qty${i}`);
+    itemPrice[i].setAttribute("id", `price${i}`);
+    const itemQty = document.getElementById(`qty${i}`);
+    const singleItemPrice = parseFloat(cartItems[i].FinalPrice);
+    totalPrice += singleItemPrice;
+
+    const itemTotalPriceTag = document.getElementById(`price${i}`);
+    itemQty.addEventListener("change", () => {
+      const itemTotalPrice = parseInt(itemQty.value) * singleItemPrice;
+      itemTotalPriceTag.innerHTML = itemTotalPrice.toString();
+
+      totalPrice = 0;
+      for (let j = 0; j < cartItems.length; j++) {
+        totalPrice += parseFloat(
+          document.getElementById(`price${j}`).innerText
+        );
+        totalPriceTag.innerHTML = `Total: ${totalPrice.toString()}`;
+      }
+    });
+  }
+  totalPriceTag.innerHTML = `Total: ${totalPrice.toString()}`;
+  totalPrice = 0;
 }
 
 renderCartContents();
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -22,9 +22,46 @@ export function setClick(selector, callback) {
   qs(selector).addEventListener("click", callback);
 }
 
-export function getParams(param){
+export function getParams(param) {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   const product = urlParams.get(param);
   return product
+}
+
+export function renderListWithTemplate(templateFn, parentElement, list, position = "afterbegin", clear = false) {
+  const htmlStrings = list.map(templateFn);
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
+export function renderWithTemplate(template, parentElement, data, callback) {
+  parentElement.insertAdjacentHTML("afterbegin", template);
+  if (callback) {
+    callback(data);
+  }
+}
+async function loadTemplate(path) {
+  const res = await fetch(path);
+  const template = await res.text();
+  return template;
+}
+
+// export function loadHeaderFooter() {
+//   //document.getElementById("main-header") = fetch("./public/partials/header.html").text();
+//   renderWithTemplate(fetch("./public/partials/header.html").text(), document.getElementById("main-header"));
+
+//   //document.getElementById("main-footer") = fetch("./public/partials/footer.html").text();
+//   renderWithTemplate(fetch("./public/partials/footer.html").text(), document.getElementById("main-footer"));
+
+
+export async function loadHeaderFooter() {
+  const headerTemplate = await loadTemplate("../partials/header.html");
+  const headerElement = document.querySelector("#main-header");
+  const footerTemplate = await loadTemplate("../partials/footer.html");
+  const footerElement = document.querySelector("#main-footer");
+
+  renderWithTemplate(headerTemplate, headerElement);
+  renderWithTemplate(footerTemplate, footerElement);
 }
