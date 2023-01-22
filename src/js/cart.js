e0287c222cbbfe21165d2051d3926daaa36539c2
ryder4676 <60828482+ryder4676@users.noreply.github.com>
2023-01-21 18:20:04 -0700

    added quantity in cart. needs item totals

--- src/js/cart.js
+++ src/js/cart.js
@@ -6,9 +6,29 @@ function renderCartContents() {
     const htmlItems = cartItems.map((item) => cartItemTemplate(item));
     document.querySelector(".product-list").innerHTML = htmlItems.join("");
     document.querySelector(".cart-footer").style.display = "block";
-    document.querySelector(".cart-total").innerHTML = `Total: ${calculateTotal(
-      cartItems
-    )}`;
+
+    const quantityBtns = document.querySelectorAll(".quantity-btn");
+    quantityBtns.forEach(btn => {
+      btn.addEventListener('click', e => {
+        const action = e.target.dataset.action;
+        const quantityEl = e.target.parentElement.querySelector('.quantity');
+        let quantity = parseInt(quantityEl.textContent);
+        // let totalItem = parseFloat(quantityEl * cartItems);
+        // document.querySelector(".cart-card__price").textContent = totalItem;
+        if (action === 'increment') {
+          quantity++;
+        } else {
+          if (quantity > 1) {
+            quantity--;
+          }
+        }
+        quantityEl.textContent = quantity;
+
+      });
+    });
+
+
+    document.querySelector(".cart-total").innerHTML = `Total: ${calculateTotal(cartItems)}`;
   } else {
     document.querySelector(".product-list").innerHTML = "Empty";
   }
@@ -26,11 +46,21 @@ function cartItemTemplate(item) {
     <h2 class="card__name">${item.Name}</h2>
   </a>
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
+
+  <div>
+    <p>Quantity: <span class="quantity">1</span></p>
+    
+    <button class="quantity-btn" data-action="increment">+</button>
+    <button class="quantity-btn" data-action="decrement">-</button>
+  </div>
+
   <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
+  </li>`;
 
   return newItem;
+}
+function calculateItemTotal() {
+
 }
 
 function calculateTotal(cartItems) {
@@ -40,5 +70,7 @@ function calculateTotal(cartItems) {
   }
   return total;
 }
+
+
 
 renderCartContents();
